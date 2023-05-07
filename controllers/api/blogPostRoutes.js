ce91dbc29b8ebfe8bded51b13c20faeeaf80c463
David C <dchung20315@gmail.com>
2023-05-07 15:50:00 -0700

    added authorizations to routes that require it

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const { BlogPost, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+router.post('/', withAuth, async (req, res) => {
     try {
         const newBlogPost = await BlogPost.create({
             ...req.body,
@@ -14,7 +15,7 @@ router.post('/', async (req, res) => {
     }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', withAuth, async (req, res) => {
     try {
         const updatedBlogPost = await BlogPost.update({
             ...req.body,
@@ -27,7 +28,7 @@ router.put('/:id', async (req, res) => {
     }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', withAuth, async (req, res) => {
     try {
         const blogPostData = await BlogPost.findByPk({
             where: {
@@ -49,7 +50,7 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', withAuth, async (req, res) => {
     try {
         const blogPostData = await BlogPost.destroy({
             where: {
